30207e74afb41bba9e69e8b31f7d8ecc689c9472
Jarrod Medrano <jmedran@gmail.com>
2019-09-21 10:48:04 -0600

    adding create task to the dto

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -46,15 +46,7 @@ export class TasksService {
   }
 
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    const { title, description } = createTaskDto;
-
-    const task = new Task();
-    task.title = title;
-    task.description = description;
-    task.status = TaskStatus.OPEN;
-    await task.save();
-
-    return task;
+    return this.taskRepository.createTask(createTaskDto);
   }
 
   // getTaskById(id: string): Task {
