4d761fc5049d301793ed4d9d2a0458e0e9e6f39e
Jarrod Medrano <jmedran@gmail.com>
2019-10-05 13:24:22 -0600

    add authorization to delete and update tasks

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -8,9 +8,13 @@ import { User } from '../auth/user.entity';
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
 
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(
+    filterDto: GetTasksFilterDto,
+    user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
+
+    query.where('task.userId = :userId', { userId: user.id });
 
     if (status) {
       query.andWhere('task.status = :status', { status });
@@ -24,7 +28,9 @@ export class TaskRepository extends Repository<Task> {
     return tasks;
   }
 
-  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+  async createTask(
+    createTaskDto: CreateTaskDto,
+    user: User): Promise<Task> {
     const { title, description } = createTaskDto;
 
     const task = new Task();
